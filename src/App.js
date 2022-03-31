b9b4fba8c1a5da6eb7708d212ee3a1e5416cad5c
Seralyn Campbell <seralyncampbell@gmail.com>
2022-03-31 15:40:44 +0900

    Added several new components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -29,7 +30,7 @@ function App() {
   ];
   return (
     <div>
-      <h2 style={{ color: "white" }}>Expense Tracker</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
